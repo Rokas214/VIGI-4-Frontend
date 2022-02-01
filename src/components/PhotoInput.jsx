101af1aff5c41ac08eaa0b9b515b20c6e790bc22
Rokas214 <rokas.guzys@gmail.com>
2022-02-01 22:34:55 +0200

    Fetch data from back end

--- src/components/PhotoInput.jsx
+++ src/components/PhotoInput.jsx
@@ -1,25 +1,35 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Card from './Card';
 
 
 
-const PhotoInput = ({getSrc,setSrc}) => {
- 
-    const image = useRef()
-
+const PhotoInput = ({getSrc,setSrc,getInputs, setInputs}) => {
+    
     function getData(){
         const src = image.current.value
-        setSrc(src)
+        setInputs(src)
+
     }
+    
+    useEffect(() => {
+        fetch("http://localhost:8080")
+            .then(res => res.json())
+            .then(data => setSrc(data))
+    },[])
+    const image = useRef()
 
   return (
     <div>
-        <input type="text" placeholder='Enter image url' ref={image} />
-        <button onClick={getData} >Submit</button>
-        <Card getSrc={getSrc} />
+        <form>
+            <h1>{getInputs}</h1>
+            <input type="text" onChange={getData} placeholder='Enter image url' ref={image} />
+            <button onClick={getData} >Submit</button>
+        </form>
+
+        {getSrc && getSrc.map((item) => <Card getSrc={item.image} /> )}
         
     </div>
   )
-};
+}
 
 export default PhotoInput;
